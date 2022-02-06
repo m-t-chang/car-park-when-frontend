0580d109587e66126522513ec6e903a0c3acec9d
mtchang <michaeltgchang@gmail.com>
2022-02-06 23:56:35 +0800

    Update SignUp.js

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,20 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
 const SignUp = () => {
+    const [emailInput, setEmailInput] = useState("");
+    const [pwInput, setPwInput] = useState("");
+    const [pwConfInput, setPwConfInput] = useState("");
+
+    // Callback for the submit button
+    // this will send a POST request and try to create a new account
+    function handleSubmit(e) {
+        // validate inputs
+        if (pwInput !== pwConfInput) {
+            console.log("Passwords must match");
+            return;
+        }
+
+        // send the POST request to backend
+        const body = { email: emailInput, password: pwInput };
+        console.log(body);
+    }
+
     return (
         <div>
             <h1>Sign Up Here</h1>
-            <TextField id="email" label="Email" variant="outlined" />
-            <TextField id="pw" label="Password" variant="outlined" />
             <TextField
+                type="email"
+                id="email"
+                label="Email"
+                variant="outlined"
+                value={emailInput}
+                onChange={(e) => {
+                    setEmailInput(e.target.value);
+                }}
+            />
+            <TextField
+                type="password"
+                id="pw"
+                label="Password"
+                variant="outlined"
+                value={pwInput}
+                onChange={(e) => {
+                    setPwInput(e.target.value);
+                }}
+            />
+            <TextField
+                type="password"
                 id="pwconf"
                 label="Confirm Password"
                 variant="outlined"
+                value={pwConfInput}
+                onChange={(e) => {
+                    setPwConfInput(e.target.value);
+                }}
             />
-            <Button variant="contained">Submit</Button>
+            <Button variant="contained" onClick={handleSubmit}>
+                Submit
+            </Button>
         </div>
     );
 };
