bc27b54f702df4ec29486bd63da11bbca0870a10
mtchang <michaeltgchang@gmail.com>
2022-02-11 03:11:52 +0800

    fix warnings

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
@@ -9,7 +8,6 @@ import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 
 const UserProfile = (props) => {
@@ -133,17 +131,6 @@ const UserProfile = (props) => {
         }
     }
 
-    function createData(name, calories, fat, carbs, protein) {
-        return { name, calories, fat, carbs, protein };
-    }
-
-    const rows = [
-        createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-        createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-        createData("Eclair", 262, 16.0, 24, 6.0),
-        createData("Cupcake", 305, 3.7, 67, 4.3),
-        createData("Gingerbread", 356, 16.0, 49, 3.9),
-    ];
     return (
         <div>
             <h1>User Profile</h1>
