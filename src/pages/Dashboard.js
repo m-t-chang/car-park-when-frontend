554d134d9506a05ee5f941960ded6e45d2b54126
mtchang <michaeltgchang@gmail.com>
2022-02-10 16:42:47 +0800

    placeholder bar chart

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,15 +5,25 @@ import {
     Chart as ChartJS,
     LineElement,
     PointElement,
+    BarElement,
     LinearScale,
     CategoryScale,
     Title,
+    Tooltip,
 } from "chart.js";
-import { Line } from "react-chartjs-2";
+import { Line, Bar } from "react-chartjs-2";
 
 import CarparkMap from "../components/CarparkMap";
 
-ChartJS.register(LineElement, PointElement, LinearScale, CategoryScale, Title);
+ChartJS.register(
+    LineElement,
+    PointElement,
+    BarElement,
+    LinearScale,
+    CategoryScale,
+    Title,
+    Tooltip
+);
 
 const Dashboard = (props) => {
     const [data, setData] = useState(null);
@@ -21,7 +31,21 @@ const Dashboard = (props) => {
     const [dataCarparkDetail, setDataCarparkDetail] = useState({});
     const [chartData, setChartData] = useState(false);
     const [carparkList, setCarparkList] = useState("");
+    const [barChartData, setBarChartData] = useState(false);
     const [locationData, setLocationData] = useState([1.3521, 103.8198]); // this is [lat,lon]
+
+    const barChartOptions = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: "top",
+            },
+            title: {
+                display: true,
+                text: "Chart.js Bar Chart",
+            },
+        },
+    };
 
     // callback to get user geolocation
     function getUserLocation() {
@@ -144,6 +168,46 @@ const Dashboard = (props) => {
                 },
             ],
         });
+
+        setBarChartData({
+            labels: [
+                "12 AM",
+                "1 AM",
+                "2 AM",
+                "3 AM",
+                "4 AM",
+                "5 AM",
+                "6 AM",
+                "7 AM",
+                "8 AM",
+                "9 AM",
+                "10 AM",
+                "11 AM",
+                "12 PM",
+                "1 PM",
+                "2 PM",
+                "3 PM",
+                "4 PM",
+                "5 PM",
+                "6 PM",
+                "7 PM",
+                "8 PM",
+                "9 PM",
+                "10 PM",
+                "11 PM",
+            ],
+
+            datasets: [
+                {
+                    label: "Dataset 1",
+                    data: [
+                        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+                        17, 18, 19, 20, 21, 22, 23, 24,
+                    ],
+                    backgroundColor: "rgba(0, 99, 132, 0.9)",
+                },
+            ],
+        });
     }, []);
 
     return (
@@ -166,6 +230,11 @@ const Dashboard = (props) => {
                 <div>
                     Please <Link to="/signin">sign in</Link> to view data
                 </div>
+            )}
+            {barChartData ? (
+                <Bar options={barChartOptions} data={barChartData} />
+            ) : (
+                ""
             )}
         </div>
     );
